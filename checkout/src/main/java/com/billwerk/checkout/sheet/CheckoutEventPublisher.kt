9ab45c8b0a3bcf27de2145d6f253a96f18006c6c
Johnny <108516218+johnnyrpy@users.noreply.github.com>
2024-07-11 11:59:38 +0200

    feat(BP-1243): add user event card_input_change (#34)
    * feat(BP-1243): add user event card_input_change
    
    * Update checkout-release.aar

--- checkout/src/main/java/com/billwerk/checkout/sheet/CheckoutEventPublisher.kt
+++ checkout/src/main/java/com/billwerk/checkout/sheet/CheckoutEventPublisher.kt
@@ -1,8 +1,10 @@
 package com.billwerk.checkout
 
+import android.util.Log
 import android.webkit.JavascriptInterface
 import com.billwerk.checkout.sheet.SDKEventMessage
 import com.billwerk.checkout.sheet.SDKEventType
+import com.billwerk.checkout.sheet.SDKUserEventMessage
 import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,10 +16,16 @@ import kotlinx.coroutines.launch
 class CheckoutEventPublisher {
     private object CheckoutEventBus {
         private val _events = MutableSharedFlow<SDKEventMessage>()
+        private val _userEvents = MutableSharedFlow<SDKUserEventMessage>()
         val events = _events.asSharedFlow()
+        val userEvents = _userEvents.asSharedFlow()
 
         suspend fun emitEvent(event: SDKEventMessage) {
             _events.emit(event)
+        }
+
+        suspend fun emitUserEvent(event: SDKUserEventMessage) {
+            _userEvents.emit(event)
         }
     }
 
@@ -25,12 +33,20 @@ class CheckoutEventPublisher {
 
         /** Broadcasts the events emitted from the Reepay checkout session  */
         val events get(): SharedFlow<SDKEventMessage> = CheckoutEventBus.events
+        val userEvents get(): SharedFlow<SDKUserEventMessage> = CheckoutEventBus.userEvents
 
         /** Emits the checkout session event */
         @JavascriptInterface
         fun postMessage(jsonMessage: String) {
-            val sdkEventMessage: SDKEventMessage = Gson().fromJson(jsonMessage, SDKEventMessage::class.java)
-            emitEvent(sdkEventMessage)
+            val message: SDKEventMessage = Gson().fromJson(jsonMessage, SDKEventMessage::class.java)
+            emitEvent(message)
+        }
+
+        /** Emits the user event */
+        @JavascriptInterface
+        fun postUserEventMessage(jsonMessage: String) {
+            val message: SDKUserEventMessage = Gson().fromJson(jsonMessage, SDKUserEventMessage::class.java)
+            emitUserEvent(message)
         }
 
         fun postSimpleEvent(event: SDKEventType) {
@@ -40,6 +56,12 @@ class CheckoutEventPublisher {
         private fun emitEvent(message: SDKEventMessage) {
             CoroutineScope(Dispatchers.Main).launch {
                 CheckoutEventBus.emitEvent(message)
+            }
+        }
+
+        private fun emitUserEvent(message: SDKUserEventMessage) {
+            CoroutineScope(Dispatchers.Main).launch {
+                CheckoutEventBus.emitUserEvent(message)
             }
         }
 
