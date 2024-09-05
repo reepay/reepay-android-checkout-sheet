4b8c9235c37e19c3d5e02cba825214745c65d73e
Asger Rud Hansen <112621087+asger-reepay@users.noreply.github.com>
2024-09-05 16:33:20 +0200

    (SUB-1741) Update url constants

--- checkout/src/main/java/com/billwerk/checkout/sheet/CheckoutSheetConstants.kt
+++ checkout/src/main/java/com/billwerk/checkout/sheet/CheckoutSheetConstants.kt
@@ -3,7 +3,8 @@ package com.billwerk.checkout.sheet
 class CheckoutSheetConstants {
     internal companion object {
         const val DOMAIN = "https://checkout.reepay.com"
-        const val MOBILEPAY_URL_TEST = "https://pay-mt.mobilepay.dk/"
+        const val MOBILEPAY_URL_TEST = "https://pay-mt.mobilepay"
         const val VIPPS_URL_TEST = "vippsmt://"
+        const val VIPPS_URL_TEST_PROD = "https://apitest.vipps"
     }
 }
