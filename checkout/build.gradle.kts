49ecca0f366cde9d58116e054b5839330bb10fef
johnnyrpy <johnny.ly@frisbii.com>
2025-10-13 11:25:01 +0200

    Update build.gradle.kts

--- checkout/build.gradle.kts
+++ checkout/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 buildscript {
-    val kotlinVersion = "2.2.10"
+    val kotlinVersion = "2.2.20"
 
     repositories {
         google()
@@ -10,7 +10,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.12.1")
+        classpath("com.android.tools.build:gradle:8.12.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
@@ -72,8 +72,8 @@ dependencies {
     api("androidx.webkit:webkit:1.14.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.code.gson:gson:2.13.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.code.gson:gson:2.13.2")
+    implementation("com.google.android.material:material:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
